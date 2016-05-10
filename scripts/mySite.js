6e233e271a938cdd6b19852dc46991ddec57171d
Dev Jana <devjana@Devs-MacBook-Pro.local>
2016-05-10 13:39:48 -0500

    adding dom append

--- scripts/mySite.js
+++ scripts/mySite.js
@@ -1,14 +1,28 @@
 // mySite jQuery example
+var currentUpdate;
+
 $(function ()
 {
-   // after the "#" is the id of the object on which we can click
-   $('#clickMeForSomething').click(function(){
-        console.log("Something happened");
-    });
-
-  $('#selectValue').change(function(){
-    console.log( "selectValue: " + $('#selectValue').val() );
+  // after the "#" is the id of the object on which we can click
+  $('#clickMeForSomething').click(function(){
+    currentUpdate = $('#containerName').val() + " is " + $('#selectFill').val() + " full of it's " + $('#selectCapacity').val() + " capacity.";
+    console.log( currentUpdate );
+    $('#outputText').append( '<p>' + currentUpdate + '</p>');
   });
+
+  $('#containerName').keyup(function(){
+    console.log( "containerName: " + $('#containerName').val() );
+  });
+
+  $('#selectFill').change(function(){
+    console.log( "selectFill: " + $('#selectFill').val() );
+  });
+
+  $('#selectCapacity').change(function(){
+    console.log( "selectCapacity: " + $('#selectCapacity').val() );
+  });
+
+
 
 });
 
