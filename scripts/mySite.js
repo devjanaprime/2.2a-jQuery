c0f63a9d751453709a9d6239a226638d5359936a
Dev Jana <devjana@Devs-MacBook-Pro.local>
2016-05-10 14:05:06 -0500

    added the Flash

--- scripts/mySite.js
+++ scripts/mySite.js
@@ -6,12 +6,17 @@ $(function ()
 {
   // after the "#" is the id of the object on which we can click
   $('#clickMeForSomething').click(function(){
-    // oncrement counter
-    counter++;
-    // calculate actual amt
-    var amount =  $('#selectFill').val() * $('#selectCapacity').val();
-    currentUpdate = $('#containerName').val() + "(id:" + counter + ") is " + $('#selectFill').val() + " full of it's " + $('#selectCapacity').val() + " capacity: " + amount;
-    $('#outputText').append( '<p>' + currentUpdate + '</p>');
+    if( $('#containerName').val() == '' ){
+     $('#containerName').fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+     }
+     else{
+      // oncrement counter
+      counter++;
+      // calculate actual amt
+      var amount =  $('#selectFill').val() * $('#selectCapacity').val();
+      currentUpdate = $('#containerName').val() + "(id:" + counter + ") is " + $('#selectFill').val() + " full of it's " + $('#selectCapacity').val() + " capacity: " + amount;
+      $('#outputText').append( '<p>' + currentUpdate + '</p>');
+    }
   });
 
   $('#containerName').keyup(function(){
